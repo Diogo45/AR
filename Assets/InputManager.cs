0c4f4b4c52fa2fa9d3d5bb0d364e525fb0eb7f7f
GAMAURER <guilherme.maurer@acad.pucrs.br>
2019-11-17 12:54:48 -0300

    touch is ready

--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Lean.Touch;
 
 public class InputManager : MonoBehaviour
 {
@@ -12,24 +13,29 @@ public class InputManager : MonoBehaviour
     }
     private void OnMouseDown()
     {
+
         dragging = true;
+        GetComponent<LeanDragTranslate>().enabled = true;
     }
     private void OnMouseUp()
     {
         dragging = false;
+        GetComponent<LeanDragTranslate>().enabled = false;
     }
 
 
     // Update is called once per frame
     void Update()
     {
+        /*
         if (dragging)
         {
+            
             Vector3 posn = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            posn.y = 0;
+            //posn.y = 0;
             transform.position = posn;
         }
-        /*if (Input.touchCount>0) {
+        if (Input.touchCount>0) {
             Touch touch = Input.GetTouch(0);
             Ray ray = Camera.main.ScreenPointToRay(touch.position);
             RaycastHit hit;
