dc7756432006f6cf0736bdb46aa86d8297b60f43
GAMAURER <guilherme.maurer@acad.pucrs.br>
2019-11-17 11:55:20 -0300

    added input manager scipt gave up on lean touch

---
+++ Assets/InputManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    bool dragging = false;
+    void Start()
+    {
+        
+    }
+    private void OnMouseDown()
+    {
+        dragging = true;
+    }
+    private void OnMouseUp()
+    {
+        dragging = false;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dragging)
+        {
+            Vector3 posn = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            posn.y = 0;
+            transform.position = posn;
+        }
+        /*if (Input.touchCount>0) {
+            Touch touch = Input.GetTouch(0);
+            Ray ray = Camera.main.ScreenPointToRay(touch.position);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit)) {
+                Debug.Log(hit.transform.name);
+                hit.transform.Translate()
+            }
+        }
+        */
+
+    }
+}
