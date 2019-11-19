e6ce6843388298dd2c4dfaa0834fc7a0df728182
Diogo45 <diogohartmann45@gmail.com>
2019-11-19 13:45:49 -0200

    Added a grid visualization

--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -25,26 +25,5 @@ public class InputManager : MonoBehaviour
 
 
     // Update is called once per frame
-    void Update()
-    {
-        /*
-        if (dragging)
-        {
-            
-            Vector3 posn = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            //posn.y = 0;
-            transform.position = posn;
-        }
-        if (Input.touchCount>0) {
-            Touch touch = Input.GetTouch(0);
-            Ray ray = Camera.main.ScreenPointToRay(touch.position);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit)) {
-                Debug.Log(hit.transform.name);
-                hit.transform.Translate()
-            }
-        }
-        */
-
-    }
+    
 }
